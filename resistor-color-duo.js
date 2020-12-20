272ceecfbef9de55f4d208fdabd21aed9bf20576
mahammad_mansur <54464015+mahammadmansur95@users.noreply.github.com>
2020-12-20 21:14:05 +0530

    Add files via upload

---
+++ resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (arr) => {
+  const COLORS = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+  
+  return +`${COLORS.indexOf(arr[0])}${COLORS.indexOf(arr[1])}`;
+};
